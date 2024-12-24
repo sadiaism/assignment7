f942f7ba43654b842f1415bae49dc292444d623b
sadiaism <ismailmoiz80@gmail.com>
2024-12-24 19:29:02 +0500

    assignment no.7

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+   
 
 
      
-      <div className="flex flex-col justify-center items-center gap-[50px] mt-[200px]">
+      <div className="flex flex-col justify-center items-center gap-[50px] mt-[100px]">
 
         <h1 className="text-[30px] text-center">Select Methods To Explore Data Fetching using Next.js</h1>
         <Link href="/client-side-fetching"><button className=" w-[200px] h-[50px] border-[1px] border-[#000000] rounded-lg hover:bg-slate-200">Client-Side-Fetching</button></Link>
@@ -24,6 +24,6 @@ export default function Home() {
 
 
 
-    </div>
+    
       );
 }
